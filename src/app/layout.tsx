8375852d6886c8b65327ac13781555aa315bdac5
anish ummenthala <ummenthala.anish@gmail.com>
2025-05-08 16:37:39 -0700

    chore: nextjs seo setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,58 @@
 import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/utils/cn";
+
+import { siteConfig } from "@/constants/site-config";
 
 import "./globals.css";
 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 export const metadata: Metadata = {
-  title: "Anish Ummenthala",
-  description: "Anish's Portfolio",
+  title: {
+    default: siteConfig.title,
+    template: `%s - ${siteConfig.title}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon/favicon.ico",
+    shortcut: "/favicon/192x192.png",
+    apple: "/favicon/apple-icon.png",
+  },
+  openGraph: {
+    url: siteConfig.url,
+    title: siteConfig.title,
+    description: siteConfig.description,
+    siteName: siteConfig.title,
+    type: "website",
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.title,
+    description: siteConfig.description,
+  },
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Anish" />
+      </head>
+      <body
+        className={cn("bg-black text-white antialiased", fontSans.variable)}
+      >
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/constants/site-config.ts
@@ -0,0 +1,11 @@
+export type SiteConfig = {
+  title: string;
+  description: string;
+  url: string;
+};
+
+export const siteConfig: SiteConfig = {
+  title: "Anish Ummenthala",
+  description: "Anish's Portfolio",
+  url: "https://uanish.com",
+};
