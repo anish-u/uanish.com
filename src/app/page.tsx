a6245bc25259ff2ed819e7a7130078d880451a1f
anish ummenthala <ummenthala.anish@gmail.com>
2025-05-08 15:21:53 -0700

    chore: shadcn/ui setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="">Hello World</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
